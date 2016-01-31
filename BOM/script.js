4efe2197639313b7a4aacb95a4331cf604b592d8
classicboy <classicboy189@gmail.com>
2016-01-31 23:24:19 +0700

    lan 1

---
+++ BOM/script.js
@@ -0,0 +1,35 @@
+﻿var x = 0;
+
+function cau1(){
+	x+= 1;
+}
+
+function cau2(){
+	x+= 1;
+}
+
+function cau3(){
+	x+= 1;
+}
+
+function cau4(){
+	x+= 1;
+}
+
+function cau5(){
+	x+= 1;
+}
+
+function cau6(){
+	x+= 1;
+}
+
+function ketqua(){
+	if(x==6){
+		window.location.href="congrat.html"
+	}else{
+		alert("POOR YOU !!!!!!!!!!! LOSERRRRRRRRRRRRR ")
+		window.location.reload("home.html")
+	}
+}
+
